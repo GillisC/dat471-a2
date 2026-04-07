bc4373ebe661b2ed8b544fad9ca6274b8c2b74fb
timca <timca@minerva.cse.chalmers.se>
2026-04-07 14:44:11 +0200

    yo

--- create_plots.py
+++ create_plots.py
@@ -8,15 +8,8 @@ df_e = pd.read_csv("results_e.csv")
 max_theo_speedup = 6.135
 
 # PLOT E
-df_d["sequential_time"] = df_d["total_time"] - df_d["parallel_time"]
-df_d["parallel_portion"] = df_d["parallel_time"] - df_d["total_time"]
-df_d["sequential_portion"] = df_d["sequential_time"] - df_d["total_time"]
-
 t_1 = df_d.loc[df_d["workers"] == 1, "total_time"].values[0]
 df_d["speedup"] = t_1 / df_d["total_time"]
-s_time = df_d.loc[df_d["workers"] == 1, "sequential_time"].values[0]
-s_portion = s_time / t_1
-
 
 plt.figure(figsize=(10, 6.18))
 plt.plot(df_d['workers'], df_d['speedup'], marker='s', color='green', label='Actual Speedup')
@@ -33,9 +26,8 @@ plt.tight_layout()
 plt.savefig('part_d_plot.png')
 
 #PLOT D
-df_e["sequential_time"] = df_e["total_time"] - df_e["parallel_time"]
-df_e["parallel_portion"] = df_e["parallel_time"] - df_e["total_time"]
-df_e["sequential_portion"] = df_e["sequential_time"] - df_e["total_time"]
+t_1 = df_e.loc[df_e["workers"] == 1, "total_time"].values[0]
+df_e["speedup"] = t_1 / df_e["total_time"]
 
 plt.figure(figsize=(10, 6.18))
 plt.plot(df_e['workers'], df_e['speedup'], marker='s', color='green', label='Actual Speedup')
