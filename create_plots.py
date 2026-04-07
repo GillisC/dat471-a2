8bcf7f0c5d1cd95a09ad192718d7862a31bca6ff
timca <timca@minerva.cse.chalmers.se>
2026-04-07 14:15:40 +0200

    yo

--- create_plots.py
+++ create_plots.py
@@ -4,12 +4,23 @@ import pandas as pd
 
 df = pd.read_csv("results_parallel_huge.csv")
 
+df["sequential_time"] = df["total_time"] - df["parallel_time"]
+df["parallel_portion"] = df["parallel_time"] - df["total_time"]
+df["sequential_portion"] = df["sequential_time"] - df["total_time"]
+
 t_1 = df.loc[df["workers"] == 1, "total_time"].values[0]
 df["speedup"] = t_1 / df["total_time"]
 
+s_time = df.loc[df["workers"] == 1, "sequential_time"].values[0]
+s_portion = s_time / t_1
+
+max_theo_speedup = 1 / s_portion
+
 plt.subplot(1, 2, 2)
 plt.plot(df['workers'], df['speedup'], marker='s', color='green', label='Actual Speedup')
-plt.plot(df['workers'], df['workers'], color='gray', linestyle=':', label='Ideal (Linear)')
+plt.axhline(y=max_theo_speedup, color='red', linestyle='--', 
+            label=f'Amdahl Limit ({max_theo_speedup:.2f}x)')
+
 plt.xlabel('Number of Workers')
 plt.ylabel('Speedup Factor')
 plt.title('Performance Scaling (Speedup)')
